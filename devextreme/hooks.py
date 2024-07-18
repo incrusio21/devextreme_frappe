530a24963e269e81afda1311ebc58cf4b4deef61
incrusio21 <abdul.malik.ahmad997@gmail.com>
2024-07-18 13:49:12 +0000

    init: init aplikasi devextreme untuk frappe

--- devextreme/hooks.py
+++ devextreme/hooks.py
@@ -10,7 +10,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/devextreme/css/devextreme.css"
-# app_include_js = "/assets/devextreme/js/devextreme.js"
+app_include_js = "dev-extreme-report.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/devextreme/css/devextreme.css"
